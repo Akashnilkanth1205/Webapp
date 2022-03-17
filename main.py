6ddf2dfa30a9e2aa81ab020f264bfd01f50f9064
Mukund666 <mukund.choudhary14@gmail.com>
2022-03-18 04:25:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def Apply_Filter(sig):
 def Plot_Graph(filtered):
    t = np.linspace(0, 30,3000, False)
    t = t[:filtered.size]
-   fig = px.line(x=t, y=filtered, labels={'x':'Time', 'y':'Amplitude'},title='Time Series', width = 800, height =400)
+   fig = px.line(x=t, y=filtered, labels={'x':'Time', 'y':'Amplitude'},title='Time Series', width = 1000, height = 600)
    st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
    
