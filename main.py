16b94db40e5dce88c093eea9ef9156b3889b6b9a
Mukund666 <mukund.choudhary14@gmail.com>
2022-03-18 04:14:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from firebase import  firebase
 from scipy import signal
 import matplotlib.pyplot as plt
-#import plotly.graph_objects as go
+import plotly.graph_objects as go
 import plotly.express as px
 import numpy as np
 import os
